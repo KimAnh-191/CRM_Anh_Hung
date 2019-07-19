af400b6a00df97723ac8418ba845d00daf778407
KimAnh <kimanh.bk10@gmail.com>
2019-07-19 18:32:50 +0700

    test

--- src/test/java/Tests/CRM_Edit_UserProfile_TC.java
+++ src/test/java/Tests/CRM_Edit_UserProfile_TC.java
@@ -49,7 +49,7 @@ public class CRM_Edit_UserProfile_TC {
         driver.get(properties.getProperty("SiteURL"));
         System.out.println(driver.getTitle());
         Thread.sleep(1000);
-        CRM_Login_Actions.enterUsernameAndPassword(driver,user1.getUsername(),user1.getPassword());
+        CRM_Login_Actions.enterUsernameAndPassword(driver, user1.getUsername(), user1.getPassword());
         CRM_Login_Actions.clickLoginButton(driver);
         CRM_Edit_UserProfile_Actions.clickProfilelink(driver);
     }
@@ -61,22 +61,27 @@ public class CRM_Edit_UserProfile_TC {
         CRM_Edit_UserProfile_Actions.enterName_Company_Phone(driver, user1.getName(), user1.getComapny(), user1.getPhone());
         CRM_Edit_UserProfile_Actions.clickSaveinformationButton(driver);
 
-        try {
-            Assert.assertTrue(driver.findElement(By.xpath(CRM_Edit_UserProfile_Pages.editSuccessMessage))
-                    .getAttribute("innerHTML").equalsIgnoreCase("Edit success!"));
+        if ((driver.findElement(By.xpath(CRM_Edit_UserProfile_Pages.editSuccessMessage)).getText()
+                .equalsIgnoreCase("Edit success!"))) {
             Result2Excels.saveResult2ExcelFilePassed("ResultDemo", "Result",
                     "TC_REQ_UPM_Update_03_01", "Verify that 'Edit success!' message displays when user updates information successfully");
 
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        } else {
             Result2Excels.saveResult2ExcelFileFailed("ResultDemo", "Result",
                     "TC_REQ_UPM_Update_03_01", "Verify that 'Edit success!' message displays when user updates information successfully");
-            waitMoment();
-            driver.quit();
-            assert false;
-
         }
+        waitMoment();
+        driver.quit();
+
+    }
+
+    //Verify that "Please enter your name" red message displays above Name field when leaving Name field blank
+    @Test(description = "Verify that 'Please enter your name' red message displays above Name field when leaving Name field blank")
+    public void LoginTC2() throws IOException {
+        driver.findElement(By.xpath(CRM_Edit_UserProfile_Pages.txtName)).clear();
+        CRM_Edit_UserProfile_Actions.clickSaveinformationButton(driver);
+
 
     }
 }
+
